6d748c66aa3a761d0218ef11a2fec01537d26886
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-20 10:17:15 +0500

    10.17 20.09.24

--- PaymentSystem/Program.cs
+++ PaymentSystem/Program.cs
@@ -23,77 +23,107 @@ namespace PaymentSystem
 
             Order order = new Order(id, amount);
 
-            PaymentSystem1 paymentSystem1 = new PaymentSystem1(Domain.ru, Currency.RUB);
+            PaymentSystem1 paymentSystem1 = new PaymentSystem1
+                (new MD5HashGenerator<int>(order.Id), Domain.ru, Currency.RUB);
             Console.WriteLine(paymentSystem1.GetPayingLink(order));
 
-            PaymentSystem2 paymentSystem2 = new PaymentSystem2(Domain.ru);
+            PaymentSystem2 paymentSystem2 = new PaymentSystem2
+                (new MD5HashGenerator<int>(order.Id + order.Amount), Domain.ru);
             Console.WriteLine(paymentSystem2.GetPayingLink(order));
 
-            PaymentSystem3 paymentSystem3 = new PaymentSystem3(Currency.RUB, Domain.com, secretKey);
+            PaymentSystem3 paymentSystem3 = new PaymentSystem3
+                (new SHA1HashGenerator<int>(order.Amount + order.Id + secretKey), Domain.com, Currency.RUB, secretKey);
             Console.WriteLine(paymentSystem3.GetPayingLink(order));
         }
     }
 
-    public class PaymentSystem1 : PaymentSystem
+    public interface IPaymentSystem
     {
-        private Currency _currency;
+        string GetPayingLink(Order order);
+    }
 
-        public PaymentSystem1(Domain domain, Currency currency, string name = "system1")
+    public class PaymentSystem1 : IPaymentSystem
+    {
+        private readonly IHashGenerator _hashGenerator;
+        private readonly Domain _domain;
+        private readonly Currency _currency;
+        private readonly string _name;
+
+        public PaymentSystem1(IHashGenerator hashGenerator, Domain domain, Currency currency, string name = "system1")
         {
+            _hashGenerator = hashGenerator;
             _domain = domain;
             _currency = currency;
             _name = name;
         }
 
-        public override string GetPayingLink(Order order) =>
-            $"pay.{_name.ToLower()}.{_domain}/{nameof(order)}?amount={order.Amount}{_currency}&{_hashGenerator.GetHashMD5(order.Id)}";
+        public string GetPayingLink(Order order) =>
+            $"pay.{_name}.{_domain}/order?amount={order.Amount}{_currency}&hash={_hashGenerator.GenerateHash()}";
     }
 
-    public class PaymentSystem2 : PaymentSystem
+    public class PaymentSystem2 : IPaymentSystem
     {
-        public PaymentSystem2(Domain domain, string name = "system2")
+        private readonly IHashGenerator _hashGenerator;
+        private readonly Domain _domain;
+        private readonly string _name;
+
+        public PaymentSystem2(IHashGenerator hashGenerator, Domain domain, string name = "system2")
         {
+            _hashGenerator = hashGenerator;
             _domain = domain;
             _name = name;
         }
 
-        public override string GetPayingLink(Order order) =>
-            $"order.{_name.ToLower()}.{_domain}/pay?{_hashGenerator.GetHashMD5(order.Id + order.Amount)}"; 
+        public string GetPayingLink(Order order) =>
+            $"order.{_name}.{_domain}/pay?hash={_hashGenerator.GenerateHash()}";
     }
 
-    public class PaymentSystem3 : PaymentSystem
+    public class PaymentSystem3 : IPaymentSystem
     {
-        private Currency _currency;
-        private int _secretKey;
+        private readonly IHashGenerator _hashGenerator;
+        private readonly Domain _domain;
+        private readonly Currency _currency;
+        private readonly int _secretKey;
+        private readonly string _name;
 
-        public PaymentSystem3(Currency currency, Domain domain, int secretKey, string name = "system3")
+        public PaymentSystem3(IHashGenerator hashGenerator, Domain domain, Currency currency, int secretKey, string name = "system3")
         {
-            _currency = currency;
+            _hashGenerator = hashGenerator;
             _domain = domain;
+            _currency = currency;
             _secretKey = secretKey;
             _name = name;
         }
 
-        public override string GetPayingLink(Order order) =>
-            $"{_name.ToLower()}.{_domain}/pay?amount={order.Amount}&currency={_currency}&{_hashGenerator.GetHashSHA1(order.Amount + order.Id + _secretKey)}";
+        public string GetPayingLink(Order order) =>
+            $"{_name}.{_domain}/pay?amount={order.Amount}&curency={_currency}&hash={_hashGenerator.GenerateHash()}";
     }
 
-    public abstract class PaymentSystem : IPaymentSystem
+    public class MD5HashGenerator<T> : IHashGenerator
     {
-        protected HashGenerator _hashGenerator = new HashGenerator();
-        protected Domain _domain;
-        protected string _name;
+        private readonly string _input;
 
-        public abstract string GetPayingLink(Order order);
+        public MD5HashGenerator(T input) =>
+            _input = input.ToString() ?? throw new ArgumentNullException($"{nameof(input)} is null");
+
+        public string GenerateHash() =>
+            Convert.ToBase64String(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(_input)));
     }
 
-    public class HashGenerator
+    public interface IHashGenerator
     {
-        public string GetHashMD5(int input) => 
-            $"hash={Convert.ToBase64String(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(Convert.ToString(input))))}";
+        string GenerateHash();
+    }
 
-        public string GetHashSHA1(int input) => 
-            $"hash={Convert.ToBase64String(SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(Convert.ToString(input))))}";
+    public class SHA1HashGenerator<T> : IHashGenerator
+    {
+        private readonly string _input;
+
+        public SHA1HashGenerator(T input) =>
+            _input = input.ToString() ?? throw new ArgumentNullException($"{nameof(input)} is null");
+
+        public string GenerateHash() =>
+            Convert.ToBase64String(SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(_input)));
     }
 
     public class Order
@@ -101,12 +131,11 @@ namespace PaymentSystem
         public readonly int Id;
         public readonly int Amount;
 
-        public Order(int id, int amount) => (Id, Amount) = (id, amount);
-    }
-
-    public interface IPaymentSystem
-    {        
-        string GetPayingLink(Order order);
+        public Order(int id, int amount)
+        {
+            Id = id;
+            Amount = amount;
+        }
     }
 
     public enum Domain
@@ -117,7 +146,6 @@ namespace PaymentSystem
 
     public enum Currency
     {
-        RUB = 0,
-        USD
+        RUB = 0
     }
 }
